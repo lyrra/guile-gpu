b02c56f5b87928b372a73413596c59f18663b580
larry <gudrun>
2020-03-28 16:30:42 +0100

    common

--- common.scm
+++ common.scm
@@ -9,3 +9,17 @@
 
 (define (random-uniform)
   (random:uniform *randstate*))
+
+(define* (assert expr #:optional errmsg)
+  (if (not expr)
+      (begin
+        (format #t "assert-error!~%")
+        (if errmsg
+          (format #t "  error: ~s~%" errmsg))
+        (exit))))
+
+; fix: default v should be one
+(define (array-inc! arr pos v)
+  (array-set! arr
+              (+ (array-ref arr pos) v)
+              pos))
