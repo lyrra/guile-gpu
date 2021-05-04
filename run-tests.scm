e61ff637db96777fea6e987dfe4b2307d6b9c02c
larry <gudrun>
2021-05-04 09:55:11 +0200

    bugfix: move out from bar, could use one dice

--- run-tests.scm
+++ run-tests.scm
@@ -64,4 +64,5 @@
                test-backgammon-path-edge
                test-backgammon-path-1mv test-backgammon-path-2mv
                test-backgammon-path-dual
-               test-backgammon-remove-pts)))
+               test-backgammon-remove-pts
+               test-backgammon-bar)))
